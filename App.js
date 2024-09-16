13ea4aae5a099b84c0667ed343f30a3087e5b4fc
SyedMohdFarhanamu <syedfarhan.amu@gmail.com>
2024-09-17 03:36:00 +0530

    app

--- App.js
+++ App.js
@@ -1,4 +1,19 @@
-const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},
-    React.createElement("h1",{id:"heading"},"Hello World from React")))
-    const root = ReactDOM.createRoot(document.getElementById("root"))
-    root.render(parent)
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+//React.createElement => ReactElement-Object =>HTMLElement(render)
+
+// const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},
+//     React.createElement("h1",{id:"heading"},"Hello World from React 🎉")))
+
+const heading = React.createElement('h1',{id:"heading"}, "Namaste React")
+
+// JSX - BABEL is converting to -> React.createElement => ReactElement-Object =>HTMLElement(render)
+const jsxHeading = (<h1 id="heading" 
+className="head" 
+tabIndex={1}>
+    Namaste React using JSX
+    </h1>);
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(jsxHeading)
